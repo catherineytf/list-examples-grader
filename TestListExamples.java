12300d0aeaab4d19119ed3c390d1beaa11fadeb7
catherineytf <114609344+catherineytf@users.noreply.github.com>
2022-11-13 10:27:16 -0800

    1

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,35 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  class LongString implements StringChecker{
+    public boolean checkString(String s){
+      if (s.length() > 5){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+  }
+  @Test
+  public void testFilter(){
+    StringChecker ls = new LongString();
+    ArrayList<String> input = new ArrayList<>();
+    input.add("apple");
+    input.add("banana");
+    input.add("pear");
+    input.add("strawberry");
+    input.add("blackberry");
+    ArrayList<String> expected = new ArrayList<>();
+    expected.add("banana");
+    expected.add("strawberry");
+    expected.add("blackberry");
+    assertEquals(expected,ListExamples.filter(input, ls));
+ }
+
 }
--- grade.sh
+++ grade.sh
@@ -2,6 +2,26 @@
 
 set -e
 
+# CPATH=:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar
 rm -rf student-submission
 git clone $1 student-submission
+
+FILE=student-submission/ListExamples.java
+if [[ -f "$FILE" ]]; then
+    echo "$FILE is submitted successfully."
+else
+    echo "$FILE is not found."
+fi
+
+cp TestListExamples.java student-submission/
+cd /Users/tongfeiyang/Documents/GitHub/list-examples-grader/
+
+
+# javac -cp .$CPATH *.java
+
+# java -cp .$CPATH org.junit.runner.JUnitCore TestListExamples 2 > error.txt
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
 
