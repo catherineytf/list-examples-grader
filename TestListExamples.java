211f9d5b8efcffa24cba74d2ac4320f7e50cd2fd
catherineytf <114609344+catherineytf@users.noreply.github.com>
2022-11-17 16:10:41 -0800

    1

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,35 +1,31 @@
-import static org.junit.Assert.*;
 
+import static org.junit.Assert.*;
+ 
 import java.util.ArrayList;
+import java.util.*;
 
 import org.junit.*;
+ 
 
-public class TestListExamples {
-  // Write your grading tests here!
-  class LongString implements StringChecker{
-    public boolean checkString(String s){
-      if (s.length() > 5){
-        return true;
-      }
-      else{
-        return false;
-      }
+class LongString implements StringChecker{
+  public boolean checkString(String s){
+    if (s.length() > 5){
+      return true;
+    }
+    else{
+      return false;
     }
   }
-  @Test
-  public void testFilter(){
-    StringChecker ls = new LongString();
-    ArrayList<String> input = new ArrayList<>();
-    input.add("apple");
-    input.add("banana");
-    input.add("pear");
-    input.add("strawberry");
-    input.add("blackberry");
-    ArrayList<String> expected = new ArrayList<>();
-    expected.add("banana");
-    expected.add("strawberry");
-    expected.add("blackberry");
-    assertEquals(expected,ListExamples.filter(input, ls));
- }
+}
+public class TestListExamples {
+ // Write your grading tests here!
+ @Test
+ public void testFilter(){
+  List<String> input = Arrays.asList("apple", "banana", "pear", "strawberry", "blackberry");
+  StringChecker sc = new LongString();
+  List<String> expected = Arrays.asList("banana", "strawberry", "blackberry");
+  assertEquals(expected, ListExamples.filter(input, sc));
+}
 
 }
+
