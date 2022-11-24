ad0616b1cde87886f49a7548bb458bc1502e3eca
catherineytf <114609344+catherineytf@users.noreply.github.com>
2022-11-24 14:17:38 -0800

    1

--- TestListExamples.java
+++ TestListExamples.java
@@ -5,12 +5,9 @@ import java.util.ArrayList;
 import java.util.*;
 
 import org.junit.*;
- 
 
 
-public class TestListExamples {
- // Write your grading tests here!
- class LongString implements StringChecker{
+class LongString implements StringChecker{
   public boolean checkString(String s){
     if (s.length() > 5){
       return true;
@@ -21,12 +18,24 @@ public class TestListExamples {
   }
 }
 
+
+public class TestListExamples {
+ // Write your grading tests here!
+ 
  @Test
  public void testFilter(){
   List<String> input = Arrays.asList("apple", "banana", "pear", "strawberry", "blackberry");
   StringChecker sc = new LongString();
   List<String> expected = Arrays.asList("banana", "strawberry", "blackberry");
   assertEquals(expected, ListExamples.filter(input, sc));
+}
+
+@Test
+public void testMerge(){
+  List<String> input1 = Arrays.asList("a","h","k");
+  List<String> input2 = Arrays.asList("b", "n");
+  List<String> expected = Arrays.asList("a", "b", "h", "k", "n");
+  assertEquals(expected, ListExamples.merge(input1, input2));
 }
 
 }
--- grade.sh
+++ grade.sh
@@ -1,60 +1,58 @@
 # Create your grading script here
-
+ 
 #set -e
-
-CPATH=":lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+ 
 rm -rf student-submission
 git clone $1 student-submission
-
+ 
 score=0
-
-# check the existence of the file 
+ 
+# check the existence of the file
 FILE=student-submission/ListExamples.java
 SUBMISSION_FILE=ListExamples.java
-if [[ -f "$FILE" ]]; then
-    echo "$SUBMISSION_FILE is submitted successfully. [1 piont]"
-    ((score+=1))
+if [[ -f "$FILE" ]]
+then
+   echo "$SUBMISSION_FILE is submitted successfully. [1 piont]"
+   ((score+=1))
 else
-    echo "$SUBMISSION_FILE is not found."
-    echo "Grade:[$score/4]"
-    exit
+   echo "$SUBMISSION_FILE is not found."
+   echo "Grade: [$score/3]"
+   exit
 fi
+ 
 
-
-
-cp TestListExamples.java student-submission/
-cd /Users/tongfeiyang/Documents/GitHub/list-examples-grader/
-
-
+cp TestListExamples.java student-submission
+cp -rf lib student-submission
+cd student-submission
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+ 
 # check if file compiles
-javac -cp .$CPATH *.java 2> compile_error.txt
-if [[ $? -eq 0]];then
+javac -cp $CPATH *.java 2> compile_error.txt
+
+if [[ $? -eq 0 ]]; then
     echo "$SUBMISSION_FILE compiled succssfully. [1 point]"
     ((score+=1))
-
+ 
 else
-    echo "compile error"
+    echo "Compile Error"
     cat compile_error.txt
-    echo "Grade:[$score/4]"
+    echo "Grade: [$score/3]"
     exit
 fi
 
-# java -cp .$CPATH org.junit.runner.JUnitCore TestListExamples 2 > error.txt
-
-#javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
 
 #run tests
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > test_result.txt
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test_result.txt
 if [[ $? -eq 0 ]];then
-    echo "All tests passed. [1 point]"
-    ((score+=1))
-    exit 0
+   echo "All tests passed. [1 point]"
+   ((score+=1))
 else
-    echo "Some tests failed."
-    exit 1
+   echo "Some tests failed."
+   grep "Tests run" test_result.txt
 fi
-
-echo "Grade: [$score/4]"
-
-
+echo "Grade: [$score/3]"
+ 
+ 
+ 
+ 
 
